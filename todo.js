8a5101f36ea3a8531b1f1da0cb521845cf37d15d
shashankrk46 <shashankrk46@gmail.com>
2020-01-30 21:14:40 +0530

    added remove claer filert task

--- todo.js
+++ todo.js
@@ -1,6 +1,6 @@
 const form=document.querySelector('#task-form');
 const taskList=document.querySelector('.collection');
-const claerBtn=document.querySelector('.clear-tasks');
+const clearBtn=document.querySelector('.clear-tasks');
 const filter=document.querySelector('#filter');
 const taskInput=document.querySelector('#task')
 
@@ -8,8 +8,15 @@ const taskInput=document.querySelector('#task')
 loadEventListner();
 
 
+
 function loadEventListner(){
     form.addEventListener('submit',addTask);
+    // remove task evnt
+    taskList.addEventListener('click',removeTask)
+
+    clearBtn.addEventListener('click',clearTask)
+
+    filter.addEventListener('keyup',filterTask)
 }
 
 function addTask(e){
@@ -34,4 +41,34 @@ taskInput.value="";
 
 
     e.preventDefault();
+}
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('are you sure')){
+        e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+//   one way to clear tasks
+// function clearTask(){
+//     taskList.innerHTML=""
+// }
+//  faster
+function clearTask(){
+while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+}
+}
+
+function filterTask(e){
+    const text=e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item=task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text)!=-1){
+            task.style.display='block';
+        }else{
+            task.style.display='none';
+        }
+    })
 }
