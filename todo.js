8c53127be6be75444295f449c37f917c88e3ad60
shashankrk46 <shashankrk46@gmail.com>
2020-01-30 19:59:13 +0530

    added todolits projects

---
+++ todo.js
@@ -0,0 +1,37 @@
+const form=document.querySelector('#task-form');
+const taskList=document.querySelector('.collection');
+const claerBtn=document.querySelector('.clear-tasks');
+const filter=document.querySelector('#filter');
+const taskInput=document.querySelector('#task')
+
+
+loadEventListner();
+
+
+function loadEventListner(){
+    form.addEventListener('submit',addTask);
+}
+
+function addTask(e){
+  if(taskInput.value===''){
+      alert('Add a task');
+  }
+const li=document.createElement('li');
+li.className='collection-item';
+li.appendChild(document.createTextNode(taskInput.value));
+
+const link=document.createElement('a');
+link.className='delete-item secondary-content';
+link.innerHTML='<i class="fa fa-remove"></i>';
+
+li.appendChild(link);
+
+
+
+taskList.appendChild(li);
+
+taskInput.value="";
+
+
+    e.preventDefault();
+}
